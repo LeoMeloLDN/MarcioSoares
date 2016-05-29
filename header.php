6ad10a55fa47a4d8103b5d5cc9d6e38042619d85
Leo Melo <leoquel.uk@gmail.com>
2016-05-29 14:29:36 +0100

    MarcioSoares Header
    Upload from Wordpress file. Changes included were image in the header, as well as the "Book Now" button on top.

---
+++ header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package MAS
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">\
+<link href='https://fonts.googleapis.com/css?family=Poiret+One'|Quicksand:400,300' rel='stylesheet' type='text/css'>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'mas' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="booking-short">
+		<h2><a href=" http://localhost:8888/wordpressMAS/booking/">Book now</a></h2>
+			
+		</div>
+		<div class="logo">
+		<a href="http://localhost:8888/wordpressMAS/"><img src="http://localhost:8888/wordpressMAS/wp-content/uploads/2016/05/marcio_soares_logo-5-copy-inverted.png"></a>
+		</div>
+		
+		<div class="site-branding">
+			<?php
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'mas' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
